36133bbd9ad4525561dc0b353833ac7865f883d6
ni-c <willi@roamsys.com>
2013-02-22 18:23:09 +0100

    Formatter

--- controller/index.js
+++ controller/index.js
@@ -3,246 +3,296 @@
  *
  * @file controller/index.js
  * @author Willi Thiel (ni-c@ni-c.de)
- *
  */
 
-if( typeof define !== 'function') {
-	var define = require('amdefine')(module);
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
 }
 
-define(['crypto', 'cookie'], function(crypto, cookie) {
-	var controller = {};
+define([ 'crypto', 'cookie' ], function(crypto, cookie) {
+  var controller = {};
 
-	/**
-	 * Build index page from plugins
-	 */
-	function getIndexFromPlugins(app, plugins, i, content, callback) {
-		plugins[i].instance.getIndex(app, function(err, result) {
-			content = content + result;
-			i++;
-			if(i < plugins.length) {
-				getIndexFromPlugins(app, plugins, i, content, callback);
-			} else {
-				callback(null, content);
-			}
-		});
-	}
+  /**
+   * StringBuffer object
+   */
+  var StringBuffer = function() {
+    this.buffer = [];
+    this.index = 0;
+  };
 
-	/**
-	 * /
-	 */
-	controller.index = function(req, res) {
-		var content = getIndexFromPlugins(req.app, req.app.get('plugins'), 0, "", function(err, content) {
-			res.render('index', {
-				title : 'Home',
-				content : content
-			});
-		});
-	};
-	
-	/**
-	 * /login
-	 */
-	controller.login = function(req, res) {
-		// If no user exists, redirect to register
-		req.app.get('db').collection('User', function(err, u) {
-			u.find({}).toArray(function(err, r) {
-				if(r.length == 0) {
-					return res.redirect('/register');
-				} else {
-					var c = cookie.parse(req.headers.cookie);
-					if((!err) && (c.email) && (c.password)) {
-						return controller.performlogin(req, res);
-					} else {
-						return res.render('login', {
-							title : 'Login',
-							hide_menubar : true
-						});
-					}
-				}
-			});
-		});
-	};
-	
-	/**
-	 * /login
-	 */
-	controller.logout = function(req, res) {
-		req.session.user = null;
-		res.render('login', {
-			title : 'Login',
-			hide_menubar : true,
-			success : 'You are now logged out.'
-		});
-	};
-	
-	/**
-	 * POST /login
-	 */
-	controller.performlogin = function(req, res) {
+  StringBuffer.prototype = {
+    append: function(s) {
+      this.buffer[this.index++] = s;
+      return this;
+    },
 
-		var c = cookie.parse(req.headers.cookie);
-		var email = req.body.email || c.email || '';
-		var password = crypto.createHash('sha256').update(req.body.password || c.password || '').digest("hex");
+    toString: function() {
+      return this.buffer.join("");
+    }
+  };
 
-		req.app.get('db').collection('User', function(err, u) {
-			u.find({
-				'email' : email,
-				'password' : password
-			}).toArray(function(err, r) {
-				if(r.length == 0) {
-					return res.render('login', {
-						title : 'Login',
-						error : 'Login failed, wrong email/password combination.',
-						hide_menubar : true
-					});
-				} else {
-					req.session.user = r[0];
-					return res.redirect('/');
-				}
-			});
-		});
-	}
-	
-	/**
-	 * POST /settings
-	 */
-	controller.changepassword = function(req, res) {
+  /**
+   * Recursive function to build index page from all Plugins
+   * 
+   * @param app The express app
+   * @param plugins The plugin object
+   * @param i The iterator
+   * @param content The StringBuffer containing the content that has been already rendered 
+   * @param callback The callback to call after the iteration of all plugins
+   */
+  function getIndexFromPlugins(app, plugins, i, content, callback) {
+    plugins[i].instance.getIndex(app, function(err, result) {
+      content.append(result);
+      if (i < plugins.length) {
+        getIndexFromPlugins(app, plugins, ++i, content, callback);
+      } else {
+        callback(null, content);
+      }
+    });
+  }
 
-		var password = crypto.createHash('sha256').update(req.body.oldpassword || '').digest("hex");
-		var newpassword = crypto.createHash('sha256').update(req.body.newpassword || '').digest("hex");
-		var newpassword2 = crypto.createHash('sha256').update(req.body.repeatnewpassword || '').digest("hex");
+  /**
+   * GET /
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.index = function(req, res) {
+    var content = StringBuffer();
+    getIndexFromPlugins(req.app, req.app.get('plugins'), 0, content, function(err, content) {
+      res.render('index', {
+        title: 'Home',
+        content: content.toString()
+      });
+    });
+  };
 
-		if(newpassword != newpassword2) {
-			return res.render('settings', {
-				title : 'Settings',
-				error : 'New passwords did not match.'
-			});
-		} else {
-			req.app.get('db').collection('User', function(err, u) {
-				u.find({
-					'email' : req.session.user.email,
-					'password' : password
-				}).toArray(function(err, r) {
-					if(r.length == 0) {
-						return res.render('settings', {
-							title : 'Settings',
-							error : 'Old password wrong.'
-						});
-					} else {
-						r[0].password = newpassword;
-						u.save(r[0], function(err, result) {
-							return res.render('settings', {
-								title : 'Settings',
-								success : 'Your password has been changed.'
-							});
-						})
-					}
-				});
-			});
-		}
-	}
-	
-	/**
-	 * /register
-	 */
-	controller.register = function(req, res) {
-		req.app.get('db').collection('User', function(err, u) {
-			u.find({}).toArray(function(err, r) {
-				if(r.length == 0) {
-					return res.render('register', {
-						title : "Register",
-						hide_menubar : true
-					});
-				} else {
-					return res.redirect('/');
-				}
-			});
-		});
-	}
-	
-	/**
-	 * POST /register
-	 */
-	controller.performregister = function(req, res) {
-		req.app.get('db').collection('User', function(err, u) {
-			u.find({}).toArray(function(err, r) {
-				if(r.length == 0) {
-					var email = req.body.email || '';
-					var password = crypto.createHash('sha256').update(req.body.password || '').digest("hex");
-					var password2 = crypto.createHash('sha256').update(req.body.repeatpassword || '').digest("hex");
+  /**
+   * GET /login
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.login = function(req, res) {
+    // If no user exists, redirect to register
+    req.app.get('db').collection('User', function(err, u) {
+      u.find({}).toArray(function(err, r) {
+        if (r.length == 0) {
+          return res.redirect('/register');
+        } else {
+          var c = cookie.parse(req.headers.cookie);
+          if ((!err) && (c.email) && (c.password)) {
+            return controller.performlogin(req, res);
+          } else {
+            return res.render('login', {
+              title: 'Login',
+              hide_menubar: true
+            });
+          }
+        }
+      });
+    });
+  };
 
-					if(password != password2) {
-						return res.render('register', {
-							title : 'Register',
-							error : 'Passwords did not match.',
-							hide_menubar : true
-						});
-					} else {
-						if(email == '') {
-							return res.render('register', {
-								title : 'Register',
-								error : 'Please enter an email address.',
-								hide_menubar : true
-							});
-						} else {
-							req.app.get('db').collection('User', function(err, u) {
-								u.save({
-									'email' : email,
-									'password' : password
-								}, function(err, result) {
-									return res.render('login', {
-										title : 'Login',
-										success : 'Registration completed. You can now login.',
-										hide_menubar : true
-									});
-								});
-							});
-						}
-					}
-				}
-			});
-		});
-	}
-	
-	/**
-	 * /settings
-	 */
-	controller.settings = function(req, res, next) {
-		if(req.params.plugin) {
-			if (req.app.get('plugin_names').indexOf(req.params.plugin) >= 0) {
-				req.app.get('plugins').forEach(function(plugin) {
-					if(plugin.name == req.params.plugin) {
-						plugin.instance.getSettings(req.app, function(err, result) {
-							return res.render('settings-plugin', {
-								content : result,
-								plugin : req.params.plugin,
-								title : req.params.plugin + ' Settings'
-							});
-						});
-					}
-				})
-			} else {
-				return next();
-			}
-		} else {
-			return res.render('settings', {
-				title : "Settings"
-			});
-		}
-	};
+  /**
+   * GET /logout
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.logout = function(req, res) {
+    req.session.user = null;
+    res.render('login', {
+      title: 'Login',
+      hide_menubar: true,
+      success: 'You are now logged out.'
+    });
+  };
+
+  /**
+   * POST /login
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.performlogin = function(req, res) {
+
+    var c = cookie.parse(req.headers.cookie);
+    var email = req.body.email || c.email || '';
+    var password = crypto.createHash('sha256').update(req.body.password || c.password || '').digest("hex");
+
+    req.app.get('db').collection('User', function(err, u) {
+      u.find({
+        'email': email,
+        'password': password
+      }).toArray(function(err, r) {
+        if (r.length == 0) {
+          return res.render('login', {
+            title: 'Login',
+            error: 'Login failed, wrong email/password combination.',
+            hide_menubar: true
+          });
+        } else {
+          req.session.user = r[0];
+          return res.redirect('/');
+        }
+      });
+    });
+  };
+
+  /**
+   * POST /settings
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.changepassword = function(req, res) {
+
+    var password = crypto.createHash('sha256').update(req.body.oldpassword || '').digest("hex");
+    var newpassword = crypto.createHash('sha256').update(req.body.newpassword || '').digest("hex");
+    var newpassword2 = crypto.createHash('sha256').update(req.body.repeatnewpassword || '').digest("hex");
+
+    if (newpassword != newpassword2) {
+      return res.render('settings', {
+        title: 'Settings',
+        error: 'New passwords did not match.'
+      });
+    } else {
+      req.app.get('db').collection('User', function(err, u) {
+        u.find({
+          'email': req.session.user.email,
+          'password': password
+        }).toArray(function(err, r) {
+          if (r.length == 0) {
+            return res.render('settings', {
+              title: 'Settings',
+              error: 'Old password wrong.'
+            });
+          } else {
+            r[0].password = newpassword;
+            u.save(r[0], function(err, result) {
+              return res.render('settings', {
+                title: 'Settings',
+                success: 'Your password has been changed.'
+              });
+            });
+          }
+        });
+      });
+    }
+  };
+
+  /**
+   * GET /register
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.register = function(req, res) {
+    req.app.get('db').collection('User', function(err, u) {
+      u.find({}).toArray(function(err, r) {
+        if (r.length == 0) {
+          return res.render('register', {
+            title: "Register",
+            hide_menubar: true
+          });
+        } else {
+          return res.redirect('/');
+        }
+      });
+    });
+  };
+
+  /**
+   * POST /register
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.performregister = function(req, res) {
+    req.app.get('db').collection('User', function(err, u) {
+      u.find({}).toArray(function(err, r) {
+        if (r.length == 0) {
+          var email = req.body.email || '';
+          var password = crypto.createHash('sha256').update(req.body.password || '').digest("hex");
+          var password2 = crypto.createHash('sha256').update(req.body.repeatpassword || '').digest("hex");
+
+          if (password != password2) {
+            return res.render('register', {
+              title: 'Register',
+              error: 'Passwords did not match.',
+              hide_menubar: true
+            });
+          } else {
+            if (email == '') {
+              return res.render('register', {
+                title: 'Register',
+                error: 'Please enter an email address.',
+                hide_menubar: true
+              });
+            } else {
+              req.app.get('db').collection('User', function(err, u) {
+                u.save({
+                  'email': email,
+                  'password': password
+                }, function(err, result) {
+                  return res.render('login', {
+                    title: 'Login',
+                    success: 'Registration completed. You can now login.',
+                    hide_menubar: true
+                  });
+                });
+              });
+            }
+          }
+        }
+      });
+    });
+  };
+
+  /**
+   * GET /settings
+   * 
+   * @param req The request
+   * @param res The response
+   */
+  controller.settings = function(req, res, next) {
+    if (req.params.plugin) {
+      if (req.app.get('plugin_names').indexOf(req.params.plugin) >= 0) {
+        req.app.get('plugins').forEach(function(plugin) {
+          if (plugin.name == req.params.plugin) {
+            plugin.instance.getSettings(req.app, function(err, result) {
+              return res.render('settings-plugin', {
+                content: result,
+                plugin: req.params.plugin,
+                title: req.params.plugin + ' Settings'
+              });
+            });
+          }
+        });
+      } else {
+        return next();
+      }
+    } else {
+      return res.render('settings', {
+        title: "Settings"
+      });
+    }
+  };
 
   /**
    * Error 404
+   * 
+   * @param req The request
+   * @param res The response
    */
-	controller.notfound = function(req, res) {
-		res.status(404).render('404', {
-			title : '404 Not Found',
-			hide_menubar : req.session.user
-		});
-	};
- 
-	
-	return controller;
+  controller.notfound = function(req, res) {
+    res.status(404).render('404', {
+      title: '404 Not Found',
+      hide_menubar: req.session.user
+    });
+  };
+
+  return controller;
 
 });
--- heimcontrol.js
+++ heimcontrol.js
@@ -2,9 +2,8 @@
  * heimcontrol.js (https://github.com/heimcontroljs/heimcontrol.js)
  *
  * @file heimcontrol.js
- * @brief Heimcontrol.js - Homeautomation in node.js
+ * @brief heimcontrol.js - Homeautomation in node.js with the Raspberry PI
  * @author Willi Thiel (ni-c@ni-c.de)
- *
  */
 
 /**
@@ -13,156 +12,157 @@
  */
 var requirejs = require('requirejs');
 requirejs.config({
-	nodeRequire : require
+  nodeRequire: require
 });
 
 /**
  * Express
  * @see http://expressjs.com/guide.html
  */
-requirejs(['http', 'connect', 'mongodb', 'path', 'express', 'node-conf', 'socket.io', 'jade', 'cookie', 'fs', './controller'], function(http, connect, mongo, path, express, conf, socketio, jade, cookie, fs, controller) {
+requirejs([ 'http', 'connect', 'mongodb', 'path', 'express', 'node-conf', 'socket.io', 'jade', 'cookie', 'fs', './controller' ], function(http, connect, mongo, path, express, conf, socketio, jade, cookie, fs, controller) {
 
-	// Load configuration
-	var node_env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-	var config = conf.load(node_env);
+  // Load configuration
+  var node_env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+  var config = conf.load(node_env);
 
-	if(!config.port || !config.secret || !config.mongo || !config.mongo.name || !config.mongo.host || !config.mongo.port || !config.mongo.user) {
-		return console.log('\u001b[31mMissing configuration file \u001b[33mconfig/' + node_env + '.json\u001b[31m. Create configuration file or start with `NODE_ENV=development node heimcontrol.js` to use another configuration file.\033[0m');
-	}
+  if (!config.port || !config.secret || !config.mongo || !config.mongo.name || !config.mongo.host || !config.mongo.port || !config.mongo.user) {
+    return console.log('\u001b[31mMissing configuration file \u001b[33mconfig/' + node_env + '.json\u001b[31m. Create configuration file or start with `NODE_ENV=development node heimcontrol.js` to use another configuration file.\033[0m');
+  }
 
-	// Initiate express
-	var app = express();
+  // Initiate express
+  var app = express();
 
-	// Load database
-	var db = new mongo.Db(config.mongo.name, new mongo.Server(config.mongo.host, config.mongo.port, config.mongo.user, {
-		native_parser : false
-	}));
+  // Load database
+  var db = new mongo.Db(config.mongo.name, new mongo.Server(config.mongo.host, config.mongo.port, config.mongo.user, {
+    native_parser: false
+  }));
 
-	var cookieParser = express.cookieParser(config.secret);
-	var sessionStore = new connect.middleware.session.MemoryStore();
+  var cookieParser = express.cookieParser(config.secret);
+  var sessionStore = new connect.middleware.session.MemoryStore();
 
   // MongoDB
-	db.open(function(err, db) {
-		if(err) {
-			return console.log('\u001b[31mFailed to connect to MongoDB: ' + err + '\033[0m');
-		} else {
-			var server = http.createServer(app).listen(config.port, function() {
-				console.log('\u001b[32mheimcontrol.js listening on port \u001b[33m%d\033[0m', config.port);
-			});
+  db.open(function(err, db) {
+    if (err) {
+      return console.log('\u001b[31mFailed to connect to MongoDB: ' + err + '\033[0m');
+    } else {
+      var server = http.createServer(app).listen(config.port, function() {
+        console.log('\u001b[32mheimcontrol.js listening on port \u001b[33m%d\033[0m', config.port);
+      });
 
-			// socket.io
-			var io = socketio.listen(server);
-			io.configure(function() {
-				io.set('log level', 0);
-				io.set('authorization', function(data, callback) {
-					if(data.headers.cookie) {
-						var c = cookie.parse(data.headers.cookie);
-						sessionStore.get(c['heimcontrol.js'].substring(2,26), function(err, session) {
-							if(err || !session) {
-								callback('Error', false);
-							} else {
-								data.session = session;
-								callback(null, true);
-							}
-						});
-					} else {
-						callback('Unauthorized', false);
-					}
-				});
-			});
+      // socket.io
+      var io = socketio.listen(server);
+      io.configure(function() {
+        io.set('log level', 0);
+        // Permission check
+        io.set('authorization', function(data, callback) {
+          if (data.headers.cookie) {
+            var c = cookie.parse(data.headers.cookie);
+            sessionStore.get(c['heimcontrol.js'].substring(2, 26), function(err, session) {
+              if (err || !session) {
+                callback('Error', false);
+              } else {
+                data.session = session;
+                callback(null, true);
+              }
+            });
+          } else {
+            callback('Unauthorized', false);
+          }
+        });
+      });
 
-			// express
-			app.configure(function() {
-				app.set('views', __dirname + '/views');
-				app.set('view engine', 'jade');
-				app.set('jade', jade);
-				app.set('server', server);
-				app.set('sockets', io.sockets);
-				app.set('mongo', mongo);
-				app.set('db', db);
-				app.set('config', config);
-				app.use(express.favicon());
-				app.use(express.logger('dev'));
-				app.use(express.bodyParser());
-				app.use(express.methodOverride());
-				app.use(cookieParser);
-				app.use(express.session({
-					store : sessionStore,
-					key : 'heimcontrol.js'
-				}));
-				app.use(express.favicon(__dirname + '/public/heimcontrol.ico'));
-				app.use(express.static(path.join(__dirname, 'public')));
-				app.use(app.router);
-			});
+      // express
+      app.configure(function() {
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.set('jade', jade);
+        app.set('server', server);
+        app.set('sockets', io.sockets);
+        app.set('mongo', mongo);
+        app.set('db', db);
+        app.set('config', config);
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(cookieParser);
+        app.use(express.session({
+          store: sessionStore,
+          key: 'heimcontrol.js'
+        }));
+        app.use(express.favicon(__dirname + '/public/heimcontrol.ico'));
+        app.use(express.static(path.join(__dirname, 'public')));
+        app.use(app.router);
+      });
 
-			app.configure('development', function() {
-				app.use(express.errorHandler());
-			});
-			
-			// Permission check
-			is_authorized = function(req, res, next) {
-				if(!req.session.user) {
-					return res.redirect('/login');
-				} else {
-					next();
-				}
-			}
-			
-			// Routes
-			app.get('/register', controller.register);
-			app.post('/register', controller.performregister);
+      app.configure('development', function() {
+        app.use(express.errorHandler());
+      });
 
-			app.get('/login', controller.login);
-			app.post('/login', controller.performlogin);
+      // Permission check
+      var is_authorized = function(req, res, next) {
+        if (!req.session.user) {
+          return res.redirect('/login');
+        } else {
+          next();
+        }
+      };
 
-			app.get('/', is_authorized, controller.index);
+      // Routes
+      app.get('/register', controller.register);
+      app.post('/register', controller.performregister);
 
-			app.get('/settings', is_authorized, controller.settings);
-			app.post('/settings', is_authorized, controller.changepassword);
+      app.get('/login', controller.login);
+      app.post('/login', controller.performlogin);
 
-			app.get('/settings/:plugin', is_authorized, controller.settings, controller.notfound);
+      app.get('/', is_authorized, controller.index);
 
-			app.get('/logout', controller.logout);
+      app.get('/settings', is_authorized, controller.settings);
+      app.post('/settings', is_authorized, controller.changepassword);
 
-			// Plugin JS and CSS
-			app.get('/plugin/:file', function(req, res) {
-				var file = req.params.file;
-				if(file.indexOf('.css', file.length - 4) !== -1) {
-					res.sendfile(__dirname + '/node_modules/heimcontrol-' + file.substr(0, file.length - 4) + '/css/' + file);
-				}
-				if(file.indexOf('.js', file.length - 3) !== -1) {
-					res.sendfile(__dirname + '/node_modules/heimcontrol-' + file.substr(0, file.length - 3) + '/js/' + file);
-				}
-			});
+      app.get('/settings/:plugin', is_authorized, controller.settings, controller.notfound);
 
-			// Read plugins
-			var plugins = [];
-			var plugin_names = [];
-			fs.readdir(__dirname + '/node_modules', function(err, files) {
-				files.forEach(function(file) {
-					if(file.indexOf('heimcontrol-') == 0) {
-						var plugin = {
-							id : file,
-							name : file.replace('heimcontrol-', '')
-						}
-						// Initialize
-						requirejs([plugin.id], function(p) {
-							p.init(app);
-							plugin.instance = p;
-							plugin.icon = p.icon;
-						});
-						plugin_names.push(plugin.name);
-						plugins.push(plugin);
-					}
-				});
-				app.locals.plugins = plugins;
-				app.set('plugins', plugins);
-				app.set('plugin_names', plugin_names);
-			});
-			
-			// 404 Not found
-			app.all('*', controller.notfound);
-			
-		}
-	});
+      app.get('/logout', controller.logout);
+
+      // Plugin JS and CSS
+      app.get('/plugin/:file', function(req, res) {
+        var file = req.params.file;
+        if (file.indexOf('.css', file.length - 4) !== -1) {
+          res.sendfile(__dirname + '/node_modules/heimcontrol-' + file.substr(0, file.length - 4) + '/css/' + file);
+        }
+        if (file.indexOf('.js', file.length - 3) !== -1) {
+          res.sendfile(__dirname + '/node_modules/heimcontrol-' + file.substr(0, file.length - 3) + '/js/' + file);
+        }
+      });
+
+      // Read plugins
+      var plugins = [];
+      var plugin_names = [];
+      fs.readdir(__dirname + '/node_modules', function(err, files) {
+        files.forEach(function(file) {
+          if (file.indexOf('heimcontrol-') === 0) {
+            var plugin = {
+              id: file,
+              name: file.replace('heimcontrol-', '')
+            };
+            // Initialize
+            requirejs([ plugin.id ], function(p) {
+              p.init(app);
+              plugin.instance = p;
+              plugin.icon = p.icon;
+            });
+            plugin_names.push(plugin.name);
+            plugins.push(plugin);
+          }
+        });
+        app.locals.plugins = plugins;
+        app.set('plugins', plugins);
+        app.set('plugin_names', plugin_names);
+      });
+
+      // 404 Not found
+      app.all('*', controller.notfound);
+
+    }
+  });
 });
