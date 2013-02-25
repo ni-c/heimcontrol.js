de2594e4b138004dce298b0c485f970d2c4057cc
ni-c <willi@roamsys.com>
2013-02-25 21:37:46 +0100

    Added a lot of documentation

---
+++ api/api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Arduino",
+        "Controller",
+        "Gpio",
+        "PluginHelper",
+        "StringBuffer",
+        "Wakeonlan"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
